d375b49ba5281a6db26d231b2a50910bb47dd1f4
Chad Murphy <57449945+cmurphy16@users.noreply.github.com>
2026-02-28 18:05:48 -0500

    Update BRChecker.sh
    Added echoes and sleep to match other scripts

--- Other/BRChecker.sh
+++ Other/BRChecker.sh
@@ -4,10 +4,14 @@ test_urls() {
     for url in ${urls[@]}; do
         response=$(curl --write-out "%{http_code}" --silent --output /dev/null "$url")
         if [[ $response == 200 ]]; then
+            echo "$response: It's up!"
             xdg-open $url
             echo $url | wl-copy
             found_url="true"
+        else
+            echo "$response: Not up yet."
         fi
+        sleep 1
     done
 }
 
