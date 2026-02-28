a05d29c9395b60cc3cc5bbd8074c35a5c3a17139
Chad Murphy <57449945+cmurphy16@users.noreply.github.com>
2026-02-28 17:27:30 -0500

    Create BRChecker.sh
    Recreated in bash for fun

---
+++ Other/BRChecker.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+test_urls() {
+    for url in ${urls[@]}; do
+        response=$(curl --write-out "%{http_code}" --silent --output /dev/null "$url")
+        if [[ $response == 200 ]]; then
+            xdg-open $url
+            echo $url | wl-copy
+            found_url="true"
+        fi
+    done
+}
+
+br_date=$(date +"%B-%-d-%Y"); br_date=${br_date,,}
+base_url="https://magic.wizards.com/en/news/announcements"
+urls=(
+    "$base_url/banned-and-restricted-$br_date"
+    "$base_url/$br_date-banned-and-restricted"
+    "$base_url/banned-and-restricted-announcement-$br_date"
+    "$base_url/$br_date-banned-and-restricted-announcement"
+)
+
+found_url="false"
+while [[ $found_url != "true" ]]; do
+    test_urls
+done
